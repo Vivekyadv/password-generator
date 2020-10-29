ca81a5598ca098b87edb7175952945da8a1c2e9c
Vivekyadv <vivek.yadav2611@gmail.com>
2020-10-29 14:51:59 +0530

    Home page

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import Job
+import random
 
-# Create your views here.
+def home(request, *args, **kwargs):
+	jobs = Job.objects
+	return render(request,'jobs/home.html', {'jobs':jobs})
+
+def gen_pswrd(request, *args, **kwargs):
+	return render (request, 'jobs/gen_pswrd.html')
+
+def password(request, *args, **kwargs):
+
+	characters = list('abcdefghijklmnopqrstuvwxyz')
+	if request.GET.get('uppercase'):
+		characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+	if request.GET.get('special'):
+		characters.extend(list('!@#$%^&*()_+|-='))
+
+	if request.GET.get('numbers'):
+		characters.extend(list('1234567890'))
+
+
+	length = int(request.GET.get('length', 12))
+	thepassword = ''
+
+	for x in range(length):
+		thepassword += random.choice(characters)
+
+	return render (request, 'jobs/password.html', {'password': thepassword})
--- password_generator/urls.py
+++ password_generator/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from jobs.views import home, password, gen_pswrd
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', home, name='home'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
